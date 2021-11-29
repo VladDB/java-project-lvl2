2669c782afcc116ebacea86247d98f5aeb8426cb
Vlad Burdin <vladyka970@gmail.com>
2021-11-29 21:54:53 +0300

    picocli, add parameters & format

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,15 +3,26 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
 class VersionedCommand {
 
-  @Option(names = { "-V", "--version" }, versionHelp = true,
-          description = "print version information and exit")
-  boolean versionRequested;
+  @Parameters(paramLabel = "filepath1", description = "path to first file")
+    File file1;
+
+  @Parameters(paramLabel = "filepath2", description = "path to second file")
+    File file2;
+
+  @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    String format;
+
+  @Option(names = { "-V", "--version" }, versionHelp = true, description = "print version information and exit")
+    boolean versionRequested;
 
   @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
   boolean usageHelpRequested;
