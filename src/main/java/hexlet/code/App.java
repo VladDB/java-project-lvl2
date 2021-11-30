5585a68c1f66ca7258216f1851043f0103178908
Vlad Burdin <vladyka970@gmail.com>
2021-11-30 22:58:19 +0300

    add call()

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,44 +6,30 @@ import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 import java.io.File;
+import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 
-class VersionedCommand {
+public class App implements Callable {
 
   @Parameters(paramLabel = "filepath1", description = "path to first file")
-    File file1;
+  private File file1;
 
   @Parameters(paramLabel = "filepath2", description = "path to second file")
-    File file2;
+  private File file2;
 
   @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
-    String format;
+  private String format;
 
-  @Option(names = { "-V", "--version" }, versionHelp = true, description = "print version information and exit")
-    boolean versionRequested;
+  @Override
+  public Object call() throws Exception {
+    return null;
+  }
 
-  @Option(names = {"-h", "--help"}, usageHelp = true, description = "display this help message")
-  boolean usageHelpRequested;
- }
-
-
-public class App {
   public static void main(String[] args) {
-
-    CommandLine commandLine = new CommandLine(new VersionedCommand());
-    commandLine.parseArgs(args);
-
-    if (commandLine.isUsageHelpRequested()) {
-      commandLine.usage(System.out);
-      return;
-    } else if (commandLine.isVersionHelpRequested()) {
-      commandLine.printVersionHelp(System.out);
-      return;
-    }
-
-    System.out.println("Hello, World!");
+    int exitCode = new CommandLine(new App()).execute(args);
+    System.exit(exitCode);
   }
 }
 
